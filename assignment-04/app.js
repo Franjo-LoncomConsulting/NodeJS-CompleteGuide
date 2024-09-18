a49cc9e9d945399bb1396c51272a1168ee095e75
Franjo-LoncomConsulting <franjo@loncomconsulting.com>
2024-09-18 16:28:16 +0200

    working on assignment 4 from section 6

---
+++ assignment-04/app.js
@@ -0,0 +1,24 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+const users = [];
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.get("/", (req, res, next) => {
+  res.render("index", { pageTitle: "Add User" });
+});
+
+app.get("/users", (req, res, next) => {
+  res.render("users", { pageTitle: "User", users: users });
+});
+
+app.post("/add-user", (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect("/users");
+});
+
+app.listen(3000);
