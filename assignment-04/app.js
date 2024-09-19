6532e39a747078e9a044412a114e4972ee1a1807
Franjo-LoncomConsulting <franjo@loncomconsulting.com>
2024-09-19 15:27:06 +0200

    Finished assignment 04

--- assignment-04/app.js
+++ assignment-04/app.js
@@ -9,16 +9,20 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.get("/", (req, res, next) => {
-  res.render("index", { pageTitle: "Add User" });
+  res.render("index", { pageTitle: "Add User Page" });
 });
 
 app.get("/users", (req, res, next) => {
-  res.render("users", { pageTitle: "User", users: users });
+  res.render("users", { pageTitle: "Users page", users: users });
 });
 
 app.post("/add-user", (req, res, next) => {
   users.push({ name: req.body.username });
   res.redirect("/users");
+});
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "404" });
 });
 
 app.listen(3000);
